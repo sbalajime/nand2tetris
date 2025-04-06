3b61584ad767a59d31625ed83db8e5ad0d7436cb
Balaji S <balajis@Balajis-MacBook-Pro.local>
2025-04-07 00:22:43 +0530

    Added solutions for project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -7,5 +7,50 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
-
-//// Replace this comment with your code.
+@8192
+D = A
+@max         // Total number of 16 bit words in the screen map
+M = D
+(START)
+@SCREEN
+D = A
+@screenindex // Denotes i th 16 bit word of the screen map 
+M = 0
+@KBD
+D = M
+@FILL
+D;JGT
+@CLEAR
+D;JLE
+    (FILL)
+        @SCREEN
+        D = A
+        @screenindex
+        D = D + M     // @SCREEN + @screenindex
+        A = D
+        M = -1
+        @screenindex
+        M = M + 1
+        D = M
+        @max
+        D = M - D
+        @FILL
+        D;JGT
+        @START  // Go to START to listen for keyboard input
+        D;JLE
+    (CLEAR)
+        @SCREEN
+        D = A
+        @screenindex
+        D = D + M
+        A = D
+        M = 0
+        @screenindex
+        M = M + 1
+        D = M
+        @max
+        D = M - D
+        @CLEAR
+        D;JGT
+@START
+0;JMP
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,40 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+// Set R2 to 0
+@R2
+M = 0
+// Check if R1 is 0. 
+//      if 0, go to end
+//      If gt 0, proceed
+@R1
+D = M
+@END
+D;JEQ
+
+// Initialize counter in a temp register
+@temp
+M = D
+(LOOP)
+    @R0
+    D = M
+    // Go to END if R0 is 0
+    @END
+    D;JEQ
+    @R2
+    M = D + M
+    @temp
+    M = M - 1
+    D = M
+    @LOOP
+    D;JGT
+(END)
+    @END
+    0;JMP
+
+
+
+
+
+
